7371c77c860ecaf4fc3d254d9c0ec356ddc93034
Venkat Selvam <veselvam@n7l12ggn07.agoda.local>
2023-04-07 11:13:58 +0530

    expressions

---
+++ MyFirstScalaProject/src/main/scala/lectures/class1/expression.scala
@@ -0,0 +1,45 @@
+package lectures.class1
+
+import scala.util.chaining.scalaUtilChainingOps
+
+object expression extends App{
+  var a = 1 + 2
+  println("a = 1+ 2 => evaluate and set the value of a as 3. this is called expression")
+  println("1. Mathematical Expressions")
+  println("     + , - , / , * , % , &, |, ^, >> , << , >>>> (>>>> only available is java)  ")
+  println("2. Comparison Expression")
+  println("     == , > , >= , < , <= ")
+  println("3. Logical Expression")
+  println("     !, &&, ||")
+  println("4. Other Expression")
+  println("     += , -= , *= , etc")
+  var b = 5
+  var c = a + 5 * 24 - b
+  var d = a%2 == 0
+  var e = !d && a < 34
+  c += b
+
+  //Instructions VS Expressions
+  //Everything is Scala is Expression!
+  //That means every line will return something. it will either return any values or return unit.
+  //Unit is equivalent to void in other languages
+
+  //while, println, variable - all are expressions which given unit as return types
+  //Expression which gives Unit as return type is called SideEffects
+  val wriredval = ( c = 7)
+  println(wriredval)
+  var e1 = println("venkat")
+  println(e1.getClass() + " " + e1)
+  var even = if (5%2 == 0) "even" else "odd";
+  println(even.getClass())
+  var printEven = if(5%2 == 0) println("even") else println("odd")
+  println(printEven.getClass())
+  var cnt = 10
+  var sum = 0
+  var whileVar = while(cnt > 0)
+    {
+      sum+=cnt
+      cnt-=1
+    }
+  println(whileVar.getClass())
+}
