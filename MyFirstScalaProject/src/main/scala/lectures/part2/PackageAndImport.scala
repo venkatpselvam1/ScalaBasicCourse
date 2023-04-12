661d730c003cdf1eba37d6cdb59b732a8a77564d
Venkatesan Panneerselvam <venkat.selvam@agoda.com>
2023-04-13 00:05:02 +0530

    package

--- MyFirstScalaProject/src/main/scala/lectures/part2/PackageAndImport.scala
+++ MyFirstScalaProject/src/main/scala/lectures/part2/PackageAndImport.scala
@@ -1,6 +1,16 @@
 package lectures.part2
 
+import lectures.class1.expression
+
 object PackageAndImport extends App{
+  println("when we are in a pacakge we can access all the class and object inside the package." +
+    "\n if we want to access the class form other package we need to use the imort keyword or full qualified name")
+
+  var exp = expression
+  println("Here expression is imported from lectures.class1.expression")
+  var playGround = playground.PlayGround
+  println("here we are using the fully qualified name and avoided using the import")
+
   helper
   println(g)
 }
