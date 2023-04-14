6958f8ae7b871c9e973f9bb01ea59ab714c3bb8d
Venkatesan Panneerselvam <venkat.selvam@agoda.com>
2023-04-14 17:41:15 +0530

    Arrays

--- MyFirstScalaProject/src/main/scala/lectures/part3/A08_Sequences.scala
+++ MyFirstScalaProject/src/main/scala/lectures/part3/A08_Sequences.scala
@@ -24,4 +24,24 @@ object A08_Sequences extends App{
   println(0 +: list :+ 9)
   println(0 +: list :+ 11 )
   println(list.mkString(":,:") )
+  var apples5 = List.fill(5)("appple")
+  println(apples5)
+
+  //Arrays - Mutable continuose block of address allocated
+  var arr = Array(1,2,3,4,5)
+  var emptyArr = Array.ofDim[Int](3)
+  println("Array of dim will create array with default value of the specified type")
+  emptyArr.foreach(println)
+  println(arr.foreach(x => print(x+", ")))
+  println()
+  println(arr(2))
+  arr(2) = 100
+  println(arr.mkString("<|>"))
+
+  //Array and seq
+  var seq2: Seq[Int] = Array(1,2,3,4, 5)
+  println(seq2)//
+  println("seq2: Seq[Int] = Array(1,2,3,4, 5) is of type ArraySeq. ArraySeq is advanced topic Implicit conversion")
+
+
 }
